98aff4cc221d6000ac9877201641376abab43d4b
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2025-05-11 19:07:16 -0400

    updated max token

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/", async (req, res) => {
         },
       ],
       temperature: 1,
-      max_tokens: 10000,
+      max_tokens: 4096,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
